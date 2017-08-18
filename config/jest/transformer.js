8c6a0d659fed9c4e1b285b1b33dfb97e564efdef
monkindey <monkindey@163.com>
2017-08-18 18:13:46 +0800

    delete debug code

--- config/jest/transformer.js
+++ config/jest/transformer.js
@@ -13,18 +13,6 @@ const globalPlugins = [
   return require.resolve(`babel-plugin-${plugin}`);
 });
 
-const src = require('fs').readFileSync(
-  '/Users/Roy/Documents/monkindey/pur-html/Examples/test/Link.test.js'
-);
-
-const code = transform(src, {
-  presets: [...globalPresets, require.resolve('babel-preset-es2015')],
-  plugins: [...globalPlugins],
-  babelrc: false
-}).code;
-
-// console.log(code);
-
 module.exports = babelJest.createTransformer({
   presets: globalPresets,
   plugins: globalPlugins,
